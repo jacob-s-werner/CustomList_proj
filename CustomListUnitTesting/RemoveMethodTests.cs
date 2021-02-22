9b3a5680655ac1ed77cae38176f045cfaf1bfdec
Jacob Werner <jacob.scott.werner@gmail.com>
2021-02-22 14:57:36 -0600

    Added RemoveMethodTest; adds three strings to customList, Removes variable two from list, checks to see if the count is 2.

--- CustomListUnitTesting/RemoveMethodTests.cs
+++ CustomListUnitTesting/RemoveMethodTests.cs
@@ -8,8 +8,27 @@ namespace CustomListUnitTesting
     public class RemoveMethodTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Remove_OneString_CountIsThree()
         {
+            //Arrange
+            CustomList<string> customList = new CustomList<string>();
+            string one = "One";
+            string two = "Two";
+            string three = "Three";
+
+            string expected = 2;
+            string actual;
+
+            //Act
+            customList.Add(one);
+            customList.Add(two);
+            customList.Add(three);
+
+            customList.Remove(two);
+            actual = customList.Count();
+
+            //Assert
+            Assert.AreEqual(expected,actual);
         }
     }
 }
