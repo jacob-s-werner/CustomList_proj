3f52c4260cc7291b41a5fdd81b48dfd9bf5b4321
Jacob Werner <jacob.scott.werner@gmail.com>
2021-02-22 11:28:12 -0600

    Initial commit of skeleton project. Created TestMethod for Add Method in CustomList Class.'

---
+++ CustomListUnitTesting/AddMethodTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CustomList;
+
+namespace CustomListUnitTesting
+{
+    [TestClass]
+    public class AddMethodTest
+    {
+        [TestMethod]
+        public void Add_OneString_ListAddedOneString()
+        {
+            //Arrange
+            CustomList<string> customList = new CustomList<string>();
+            string name = "Jake";
+            int expected = 1;
+            int actual;
+
+            //Act
+            customList.Add(name);
+            actual = customList.Count();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ CustomListUnitTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomListUnitTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CustomListUnitTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2b37a73e-8eb7-48ab-84c7-64eee8f12411")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
