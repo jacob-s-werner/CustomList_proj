296b06f1d95c56a421c36e4df0f584137aa41773
Jacob Werner <jacob.scott.werner@gmail.com>
2021-02-23 15:12:44 -0600

    Created MinusOperatorOverloadMethodTests Class.

---
+++ CustomListUnitTesting/MinusOperatorOverloadMethodTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CustomList;
+
+namespace CustomListUnitTesting
+{
+    [TestClass]
+    public class MinusOperatorOverloadMethodTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
