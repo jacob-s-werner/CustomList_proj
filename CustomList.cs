2f3f658c77b519c66de4dca8d522b14203dd0ec7
Jacob Werner <jacob.scott.werner@gmail.com>
2021-02-23 10:28:45 -0600

    Changed Remove Method in CustomList Class; to take in T value and return a bool, created bool removed variable starting at false, created a T[] tempArray to store the old array. Created a for loop to check for the value to remove (if it matches a value in the array it subtracts one from count and changes removed to true while skipping the addition of the matching variable to tempArray). Added an if condition after the loop to check if value was removed; if true it makes _items a new array and stores the values from tempArray, if false it just returns false. Changed Add Method to replace count in the for loops to capacity.

--- CustomList.cs
+++ CustomList.cs
@@ -49,7 +49,7 @@ namespace CustomList
             {
                 tempArray = new T[capacity];
                 
-                for (int i = 0; i < count; i++)
+                for (int i = 0; i < capacity; i++)
                 {
                     tempArray[i] = _items[i];
                 }
@@ -57,19 +57,47 @@ namespace CustomList
                 capacity *= 2;
                 _items = new T[capacity];
 
-                for (int i = 0; i < count; i++)
+                for (int i = 0; i < capacity; i++)
                 {
                     _items[i] = tempArray[i];
                 }
             }
-
             _items[count] = itemToAdd;
             count++;
         }
 
         public bool Remove(T value) 
         {
-            
+            T[] tempArray = new T[capacity];
+            bool removed = false;
+
+            for (int i = 0; i < capacity; i++)
+            {
+                if (_items[i].Equals(value) && removed == true) //1, 3, 4, 3
+                {
+                    count--;
+                    removed = true;
+                }
+                else
+                {
+                    tempArray[i] = _items[i];
+                }
+            }
+
+            if (removed == false)
+            {
+                return removed;
+            }
+            else
+            {
+                _items = new T[capacity];
+
+                for (int i = 0; i < count; i++)
+                {
+                    _items[i] = tempArray[i];
+                }
+                return removed;
+            }
         }
     }
 }
