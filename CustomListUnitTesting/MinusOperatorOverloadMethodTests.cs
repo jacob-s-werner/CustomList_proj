1e29f1b19dcdda57a5bb5cbd8552ca1223643134
Jacob Werner <jacob.scott.werner@gmail.com>
2021-02-23 15:35:44 -0600

    Created MinusOperatorOverloadMethodTest; created two int CustomLists (one and two), subtracted integers in list two from list one, matches counto the expected value of 2.

--- CustomListUnitTesting/MinusOperatorOverloadMethodTests.cs
+++ CustomListUnitTesting/MinusOperatorOverloadMethodTests.cs
@@ -33,5 +33,30 @@ namespace CustomListUnitTesting
             //Assert
             Assert.ReferenceEquals(expected, actual);
         }
+        [TestMethod]
+        public void MinusOperator_TwoListsTotalingSixIntegers_CountMatches2()
+        {
+            //Arrange
+            CustomList<int> one = new CustomList<int>();
+            CustomList<int> two = new CustomList<int>();
+            CustomList<int> result;
+            int expected = 2;
+            int actual;
+
+            //Act
+            one.Add(1);
+            one.Add(3);
+            one.Add(5);
+
+            two.Add(2);
+            two.Add(1);
+            two.Add(6);
+
+            result = one - two;
+            actual = result.Count;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
