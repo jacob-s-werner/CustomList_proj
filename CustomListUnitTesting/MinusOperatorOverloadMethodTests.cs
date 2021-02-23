3e2417bbada670c997c1d4dee2412bddf2b5768d
Jacob Werner <jacob.scott.werner@gmail.com>
2021-02-23 15:48:55 -0600

    Created MinusOperatorOverloadMethodTest; created two char lists(one and two, two being empty), removed no strings from the list, checks to see if matches expected.

--- CustomListUnitTesting/MinusOperatorOverloadMethodTests.cs
+++ CustomListUnitTesting/MinusOperatorOverloadMethodTests.cs
@@ -84,5 +84,28 @@ namespace CustomListUnitTesting
             //Assert
             Assert.AreEqual(expected, actual);
         }
+        [TestMethod]
+        public void MinusOperator_OneShortCharListAndOneEmptyCharList_NoCharsRemovedFromList()
+        {
+            //Arrange
+            CustomList<char> list = new CustomList<char>();
+            CustomList<char> empty = new CustomList<char>();
+            CustomList<char> expected = new CustomList<char>();
+            expected.Add('a');
+            expected.Add('b');
+            expected.Add('c');
+
+            CustomList<char> actual;
+
+            //Act
+            list.Add('a');
+            list.Add('b');
+            list.Add('c');
+
+            actual = list - empty;
+
+            //Assert
+            Assert.ReferenceEquals(expected, actual);
+        }
     }
 }
