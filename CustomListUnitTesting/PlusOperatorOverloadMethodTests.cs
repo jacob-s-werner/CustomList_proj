ed663ea1c5bb185fe28e44c0270df2cc33ecda02
Jacob Werner <jacob.scott.werner@gmail.com>
2021-02-23 14:01:20 -0600

    Created PlusOperatorMethodTest; Created two CustomLists of integers (one and two), created an expected CustomList, added CustomList one and two together as a new actual CustomList. Checked to see if actual is equal to expected.

--- CustomListUnitTesting/PlusOperatorOverloadMethodTests.cs
+++ CustomListUnitTesting/PlusOperatorOverloadMethodTests.cs
@@ -8,8 +8,34 @@ namespace CustomListUnitTesting
     public class PlusOperatorOverloadMethodTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void PlusOperator_TwoListsOfIntegers_CombinedTwoLists()
         {
+            //Arrange
+            CustomList<int> one = new CustomList<int>();
+            CustomList<int> two = new CustomList<int>();
+            CustomList<int> expected = new CustomList<int>();
+            expected.Add(1);
+            expected.Add(3);
+            expected.Add(5);
+            expected.Add(2);
+            expected.Add(4);
+            expected.Add(6);
+
+            CustomList<int> actual;
+
+            //Act
+            one.Add(1);
+            one.Add(3);
+            one.Add(5);
+
+            two.Add(2);
+            two.Add(4);
+            two.Add(6);
+
+            actual = one + two;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
         }
     }
 }
