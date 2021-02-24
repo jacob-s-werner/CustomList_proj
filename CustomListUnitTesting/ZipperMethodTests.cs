60cc2b2a896bf55a1cdaf213dc302decea306ad9
Jacob Werner <jacob.scott.werner@gmail.com>
2021-02-24 13:09:05 -0600

    Created ZipperMethodTest: created two int CustomLists called one and two (each holding 3 integers), created expected list to be the zippered output, checking to see if zippered correctly into CustomList one.

--- CustomListUnitTesting/ZipperMethodTests.cs
+++ CustomListUnitTesting/ZipperMethodTests.cs
@@ -5,11 +5,37 @@ using CustomList;
 namespace CustomListUnitTesting
 {
     [TestClass]
-    public class ZipperMethodTests
+    public class ZipperMethodTests : ExtraTestMethods
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Zipper_TwoListsSixIntegers_ZipperedList()
         {
+            //Arrange
+            CustomList<int> one = new CustomList<int>();
+            CustomList<int> two = new CustomList<int>();
+            CustomList<int> expected = new CustomList<int>();
+            expected.Add(1);
+            expected.Add(3);
+            expected.Add(5);
+            expected.Add(2);
+            expected.Add(4);
+            expected.Add(6);
+
+            CustomList<int> actual;
+
+            //Act
+            one.Add(1);
+            one.Add(5);
+            one.Add(4);
+
+            two.Add(3);
+            two.Add(2);
+            two.Add(6);
+
+            actual = one.Zipper(two);
+
+            //Assert
+            CheckIfTwoListsHaveIdenticalIndexes(expected, actual);
         }
     }
 }
