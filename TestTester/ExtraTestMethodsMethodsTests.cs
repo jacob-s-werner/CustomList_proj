e7e0c3b26279d80afa92349c3753f9d8b6781017
Jacob Werner <jacob.scott.werner@gmail.com>
2021-02-25 09:19:36 -0600

    Created new project TestTester; created ExtraTestMethodsMethodsTests Class to test the abstract Class ExtraTestMethods (out of curiousity); it creates two CustomLists and checks their indexes using the CheckIfTwoListsHaveIdenticalIndexes Method looking for an AssertFailedException.

---
+++ TestTester/ExtraTestMethodsMethodsTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CustomList;
+using CustomListUnitTesting;
+
+namespace TestTester
+{
+    [TestClass]
+    public class ExtraTestMethodsMethodsTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(AssertFailedException))]
+        public void CheckIfTwoListsHaveIdenticalIndexes_TwoIntegerListsDifferentSizesDuplicateArrayValues_ExceptionThrown()
+        {
+            //Arrange
+            MinusOperatorOverloadMethodTests minusTests = new MinusOperatorOverloadMethodTests();
+            CustomList<int> customList1 = new CustomList<int>();
+            CustomList<int> customList2 = new CustomList<int>();
+
+            customList1.Add(1);
+            customList2.Add(1);
+            customList2.Add(1);
+            customList2.Add(1);
+
+            //Act
+            minusTests.CheckIfTwoListsHaveIdenticalIndexes(customList1, customList2);
+            
+        }
+    }
+}
---
+++ TestTester/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestTester3000")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestTester3000")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d69bda9e-c622-4056-bfb0-f41e6e1c5c58")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
