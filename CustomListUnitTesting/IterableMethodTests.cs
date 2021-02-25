f1dd825f3f9c5c695a9a8a8be063fbd3d0118cc8
Jacob Werner <jacob.scott.werner@gmail.com>
2021-02-25 10:21:12 -0600

    Created IterableMethodTests Class

---
+++ CustomListUnitTesting/IterableMethodTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CustomList;
+
+namespace CustomListUnitTesting
+{
+    [TestClass]
+    public class IterableMethodTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
